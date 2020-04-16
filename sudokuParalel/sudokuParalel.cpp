303558446372639945748c199fca56160210f2f7
pajav7 <61421515+pajav7@users.noreply.github.com>
2020-04-16 16:49:31 +0200

    Add files via upload

--- sudokuParalel/sudokuParalel.cpp
+++ sudokuParalel/sudokuParalel.cpp
@@ -27,44 +27,31 @@ int main(const int argc, char*** argv)
 	int rowFirst = 0;
 	int collumnFirst = 0;
 	int sudoku[N][N] = { 0 };
-	int solutionRank = 0;
-	bool solutionFound = false;
-	
-	/*int sudoku[N][N] = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
-						{5, 0, 0, 0, 4, 0, 9, 0, 0},
-						{0, 9, 0, 0, 7, 0, 0, 1, 0},
-						{0, 0, 3, 4, 0, 0, 0, 5, 0},
-						{7, 0, 0, 0, 0, 0, 0, 0, 8},
-						{0, 6, 0, 0, 0, 7, 3, 0, 0},
-						{0, 2, 0, 0, 1, 0, 0, 9, 0},
-						{0, 0, 7, 0, 9, 0, 0, 0, 2},
-						{0, 0, 0, 7, 2, 0, 4, 8, 0} };*/
+	int solutionRank = world_size;
+	int solutionFound = 0;
 
 	if (world_rank == 0) {
-		/*int sudoku[N][N] = { {0,0,0,0,1,0,3,8,7},
-					{0,0,4,0,0,5,0,0,1},
-					{0,1,0,0,0,2,4,0,0},
-					{0,5,6,0,0,0,0,4,0},
-					{2,0,0,0,0,0,0,0,3},
-					{0,7,0,0,0,0,2,6,0},
-					{0,0,1,6,0,0,0,9,0},
-					{6,0,0,8,0,0,7,0,0},
-					{7,9,8,0,3,0,0,0,0} };*/
 		s.setGridFromFile(sudoku);
 	}
+
 	MPI_Bcast(*(sudoku), N * N, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
-	
-	if(world_rank==1) s.printGrid(sudoku);
 
 	if ((world_size > 1) && (world_rank == 1)) {
-		s.checkContradictions(sudoku);
+		if (s.checkContradictions(sudoku) == false) {
+			cout << "The program can not run due to some contradictions on the input" << endl;
+		}
+		s.printGrid(sudoku);
 	}
-	else if(world_size == 1){
-		s.checkContradictions(sudoku);
+	else if (world_size == 1) {
+		if (s.checkContradictions(sudoku) == false) {
+			cout << "The program can not run due to some contradictions on the input" << endl;
+		}
+
+		s.printGrid(sudoku);
 	}
 
-	if(world_rank ==0){
+	if (world_rank == 0) {
 		s.computeNumberPossibilities(sudoku, 1, 0, -1, rowFirst, collumnFirst, possibleFirst, numberPossibleFirst);
 	}
 
@@ -72,11 +59,11 @@ int main(const int argc, char*** argv)
 	MPI_Bcast(&collumnFirst, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&numberPossibleFirst, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(possibleFirst, N, MPI_INT, 0, MPI_COMM_WORLD);
-	//if (s.checkContradictions()) {
 
+	MPI_Request request;
 	MPI_Barrier(MPI_COMM_WORLD);
 
-	if (world_size > numberPossibleFirst) {
+	if (world_size >= 2 * numberPossibleFirst) {
 		int possibleSecond[N] = { 0 };
 		int numberPossibleSecond = 0;
 		int rowSecond = 0;
@@ -90,76 +77,94 @@ int main(const int argc, char*** argv)
 		MPI_Bcast(&numberPossibleSecond, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Bcast(possibleSecond, N, MPI_INT, 0, MPI_COMM_WORLD);
 
-		//cout << world_rank << " ma possibilities for second " << numberPossibleSecond << endl;
-		//cout << world_rank << " ma hodnotu rowSecond a collumnSecond " << rowSecond << collumnSecond << endl;
-
-		MPI_Barrier(MPI_COMM_WORLD);
+		//MPI_Barrier(MPI_COMM_WORLD);
 
 		int numForOne = world_size / numberPossibleFirst;
-		
+
+		int flag;
+
 		for (int posFirst = 0; posFirst < numberPossibleFirst; posFirst++) {
 			for (int posSecond = 0; posSecond < numberPossibleSecond; posSecond++) {
 				if (world_rank == posFirst * numForOne + (posSecond % numForOne)) {
 					sudoku[rowFirst][collumnFirst] = possibleFirst[posFirst];
 					sudoku[rowSecond][collumnSecond] = possibleSecond[posSecond];
-					if (s.solveSudoku(sudoku, 0, 0)) {
+					if (s.solveSudoku(sudoku)) {
 						cout << world_rank << " found solution:" << endl;
 						s.printGrid(sudoku);
 						solutionRank = world_rank;
-						solutionFound = true;
-						break;
+						solutionFound = 1;
+						for (int i = 0; i < world_size; i++) {
+							if (i != solutionRank) {
+								MPI_Isend(&solutionRank, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &request);
+							}
+						}
+						goto theEnd;
 					}
 					else {
 						cout << world_rank << " ended without solution" << endl;
 					}
 				}
-			}
-		}
-	}
-	else if (world_size == numberPossibleFirst) {
-		cout << "Pocet vlaken je presny pocet moznosti pro prvni volne policko" << endl;
-		for (int possibility = 0; possibility < numberPossibleFirst; possibility++) {
-			if (world_rank == possibility) {
-				sudoku[rowFirst][collumnFirst] = possibleFirst[possibility];
-				if (s.solveSudoku(sudoku, 0, 0)) {
-					cout << world_rank << " found solution:" << endl;
-					solutionFound = true;
-					s.printGrid(sudoku);
-					solutionRank = world_rank;
-					break;
-				}
-				else {
-					cout << world_rank << " ended without solution" << endl;
+				MPI_Iprobe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
+				if (flag != 0) {
+					cout << world_rank << " konci kvuli flag == true" << endl;
+					goto theEnd;
 				}
 			}
 		}
 	}
 	else {
-		cout << "Pocet vlaken je mensi" << endl;
+		int flag;
 		for (int possibility = 0; possibility < numberPossibleFirst; possibility++) {
 			if (world_rank == possibility % world_size) {
 				sudoku[rowFirst][collumnFirst] = possibleFirst[possibility];
-				if (s.solveSudoku(sudoku, 0, 0)) {
+				if (s.solveSudoku(sudoku)) {
+					cout << endl;
 					cout << world_rank << " found solution:" << endl;
-					solutionFound = true;
+					solutionFound = 1;
 					s.printGrid(sudoku);
 					solutionRank = world_rank;
+					for (int i = 0; i < world_size; i++) {
+						if (i != solutionRank) {
+							MPI_Isend(&solutionRank, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &request);
+						}
+					}
 					break;
 				}
 				else {
 					cout << world_rank << " ended without solution" << endl;
 				}
 			}
+			MPI_Iprobe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
+			if (flag != 0) {
+				cout << world_rank << " konci kvuli flag == true" << endl;
+				break;
+			}
 		}
 	}
+	theEnd:
+
+	/*MPI_Request request2;
+	if (world_rank == solutionRank) {
+		MPI_Isend(&solutionRank, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &request2);
+	}
+	
+	int flag2 = 0;
+	if (world_rank == 0) {
+		MPI_Iprobe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &flag2, MPI_STATUS_IGNORE);
+		if (flag2 != 0) {
+			MPI_Recv(&solutionRank, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+	}*/
 	
 	auto stop = chrono::high_resolution_clock::now(); //Duration count
-	
+
 	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-	if ((world_rank == solutionRank) && (solutionFound == true)) {
-		cout << "Total time for thread "<< world_rank << " is: " << duration.count() << " microseconds." << endl;
-		}
-	
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if (world_rank == 0) {
+		cout << "Total time for thread " << world_rank << " is: " << duration.count() << " microseconds." << endl;
+	}
 	MPI_Finalize();
+
 }
 
