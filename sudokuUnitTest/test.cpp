cbd4ad9ffec2082e6149ad674d6f504e9d3654db
pajav7 <61421515+pajav7@users.noreply.github.com>
2020-04-16 16:39:00 +0200

    Add files via upload

--- sudokuUnitTest/test.cpp
+++ sudokuUnitTest/test.cpp
@@ -6,7 +6,8 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace {
 	TEST(TestSetGrid, PositiveCase) {
-		int s[N][N] = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
+		int s[N][N];
+		int expected[N][N] = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
 						{5, 0, 0, 0, 4, 0, 9, 0, 0},
 						{0, 9, 0, 0, 7, 0, 0, 1, 0},
 						{0, 0, 3, 4, 0, 0, 0, 5, 0},
@@ -16,19 +17,21 @@ namespace {
 						{0, 0, 7, 0, 9, 0, 0, 0, 2},
 						{0, 0, 0, 7, 2, 0, 4, 8, 0} };
 		Solver solver;
-		solver.setGrid(s);
+		solver.setGridFromFile(s, "sudoku3.txt");
 		for (int i = 0; i < N; i++) {
-			EXPECT_TRUE(0 == std::memcmp(s[i], solver.getGrid()[i], N));
+			EXPECT_TRUE(0 == std::memcmp(s[i], expected[i], N));
 		}
 	}
 
 	TEST(TestContradiction, NegativeCase) {
 		Solver solver;
 		solver.setInputOK(false);
-		EXPECT_FALSE(solver.checkContradictions());
+		int checkTry[N][N] = { 0 };
+		EXPECT_FALSE(solver.checkContradictions(checkTry));
 
 		solver.setInputOK(true);
-		int s[N][N] = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
+		int s[N][N];
+		/*int s[N][N] = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
 						{5, 0, 0, 0, 4, 0, 9, 0, 0},
 						{0, 9, 0, 0, 7, 0, 0, 1, 0},
 						{0, 0, 3, 4, 0, 0, 0, 5, 0},
@@ -36,21 +39,23 @@ namespace {
 						{0, 6, 0, 0, 0, 7, 3, 0, 0},
 						{0, 2, 0, 0, 1, 0, 0, 9, 0},
 						{0, 0, 7, 0, 9, 0, 0, 0, 2},
-						{0, 0, 0, 7, 2, 0, 4, 8, 0} };
-		solver.setGrid(s);
-		EXPECT_FALSE(solver.checkContradictions());
+						{0, 0, 0, 7, 2, 0, 4, 8, 0} };*/
+		solver.setGridFromFile(s, "sudoku3.txt");
+		s[4][6] = 5;
+		EXPECT_FALSE(solver.checkContradictions(s));
 
 		s[4][6] = 7;
-		EXPECT_FALSE(solver.checkContradictions());
+		EXPECT_FALSE(solver.checkContradictions(s));
 
 		s[4][6] = 9;
-		EXPECT_FALSE(solver.checkContradictions());
+		solver.printGrid(s);
+		EXPECT_FALSE(solver.checkContradictions(s));
 	}
 
 	TEST(TestContradiction, PositiveCase) {
 		Solver solver;
 		solver.setInputOK(true);
-		int s[N][N] = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
+		int s[N][N]; /*= { {0, 3, 4, 0, 8, 2, 0, 0, 0},
 						{5, 0, 0, 0, 4, 0, 9, 0, 0},
 						{0, 9, 0, 0, 7, 0, 0, 1, 0},
 						{0, 0, 3, 4, 0, 0, 0, 5, 0},
@@ -58,14 +63,14 @@ namespace {
 						{0, 6, 0, 0, 0, 7, 3, 0, 0},
 						{0, 2, 0, 0, 1, 0, 0, 9, 0},
 						{0, 0, 7, 0, 9, 0, 0, 0, 2},
-						{0, 0, 0, 7, 2, 0, 4, 8, 0} };
-		solver.setGrid(s);
-		EXPECT_TRUE(solver.checkContradictions());
+						{0, 0, 0, 7, 2, 0, 4, 8, 0} };*/
+		solver.setGridFromFile(s, "sudoku3.txt");
+		EXPECT_TRUE(solver.checkContradictions(s));
 	}
 
 	TEST(TestSolve, PositiveCase) {
 		Solver solver;
-		int s[N][N] = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
+		int s[N][N];/* = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
 						{5, 0, 0, 0, 4, 0, 9, 0, 0},
 						{0, 9, 0, 0, 7, 0, 0, 1, 0},
 						{0, 0, 3, 4, 0, 0, 0, 5, 0},
@@ -73,9 +78,9 @@ namespace {
 						{0, 6, 0, 0, 0, 7, 3, 0, 0},
 						{0, 2, 0, 0, 1, 0, 0, 9, 0},
 						{0, 0, 7, 0, 9, 0, 0, 0, 2},
-						{0, 0, 0, 7, 2, 0, 4, 8, 0} };
-		solver.setGrid(s);
-		EXPECT_TRUE(solver.solveSudoku());
+						{0, 0, 0, 7, 2, 0, 4, 8, 0} };*/
+		solver.setGridFromFile(s, "sudoku3.txt");
+		EXPECT_TRUE(solver.solveSudoku(s));
 
 		int reseni[N][N] = { {1, 3, 4, 9, 8, 2, 6, 7, 5},
 							{5, 7, 8, 6, 4, 1, 9, 2, 3},
@@ -87,13 +92,13 @@ namespace {
 							{4, 8, 7, 5, 9, 6, 1, 3, 2},
 							{9, 5, 1, 7, 2, 3, 4, 8, 6} };
 		for (int i = 0; i < N; i++) {
-			EXPECT_TRUE(0 == std::memcmp(reseni[i], solver.getGrid()[i], N));
+			EXPECT_TRUE(0 == std::memcmp(reseni[i], s[i], N));
 		}
 	}
 
 	TEST(TestSolve, NegativeCase) {
 		Solver solver;
-		int s[N][N] = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
+		int s[N][N];/* = { {0, 3, 4, 0, 8, 2, 0, 0, 0},
 						{5, 0, 0, 0, 4, 0, 9, 0, 0},
 						{0, 9, 0, 0, 7, 0, 0, 1, 0},
 						{0, 0, 3, 4, 0, 0, 0, 5, 0},
@@ -101,9 +106,10 @@ namespace {
 						{0, 6, 0, 0, 0, 7, 3, 0, 0},
 						{0, 2, 0, 0, 1, 0, 0, 9, 0},
 						{0, 0, 7, 0, 9, 0, 0, 0, 2},
-						{0, 0, 0, 7, 2, 0, 4, 8, 0} };
-		solver.setGrid(s);
-		EXPECT_FALSE(solver.solveSudoku());
+						{0, 0, 0, 7, 2, 0, 4, 8, 0} };*/
+		solver.setGridFromFile(s, "sudoku3.txt");
+		s[4][6] = 6;
+		EXPECT_FALSE(solver.solveSudoku(s));
 	}
 
 }
